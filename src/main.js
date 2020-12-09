528dd7cd8152526dccac114c585e4262d55b4c33
myh759900469 <759900469@qq.com>
2020-12-09 23:54:35 +0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+/* 项目的启动入口 */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+// 创建 Vue 根实例
+// 把 router 配置到根实例中
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
